cd452ec2b0485a6c0d43a2ee896dacc6b87e949e
wufen <KreaWu@163.com>
2019-10-10 15:09:25 +0800

    文章的增删改查

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+//文章列表
+Route::get('/posts', '\App\Http\Controllers\PostController@index');
+
+//文章详情页我
+Route::get('/posts/{post}', '\App\Http\Controllers\PostController@show');
+
+//创建文章
+Route::get('/posts/create', '\App\Http\Controllers\PostController@create');
+Route::post('/posts', '\App\Http\Controllers\PostController@store');
+
+//修改文章
+Route::get('/posts/{post}/edit', '\App\Http\Controllers\PostController@edit');
+Route::post('/posts/{post}', '\App\Http\Controllers\PostController@update');
+
+//删除文章
+Route::get('/posts/delete', '\App\Http\Controllers\PostController@delete');
