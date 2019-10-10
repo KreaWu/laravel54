3d51da6c19e12fd9bad3c5464fee8063b4678691
wufen <KreaWu@163.com>
2019-10-10 15:19:25 +0800

    增删改查逻辑方法

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,36 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    //列表
+    public function index(){
+
+    }
+
+    //详情
+    public function show(){
+
+    }
+
+    //创建
+    public function create(){
+
+    }
+
+    public function store(){
+
+    }
+
+    //修改
+    public function edit(){
+
+    }
+
+    public function update(){
+
+    }
+
+    //删除
+    public function delete(){
+
+    }
 }
