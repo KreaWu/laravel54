0847ddbac03a48102c9fb8584402179d2c76ea28
wufen <KreaWu@163.com>
2019-10-15 18:41:36 +0800

    使用预加载减少view层的数据库访问

--- app/admin/Controllers/PostController.php
+++ app/admin/Controllers/PostController.php
@@ -7,7 +7,12 @@ class PostController extends Controller
 {
     //文章管理列表
     public function index(){
-        $posts = Post::withoutGlobalScope('avaiable')->where('status', 0)->orderBy('created_at', 'desc')->paginate(10);//表明不使用叫avaiable的全局范围
+        //使用load/with进行预加载，减少前端view层对数据库的访问
+        $posts = Post::withoutGlobalScope('avaiable')->where('status', 0)->with('user')->orderBy('created_at', 'desc')->paginate(10);//表明不使用叫avaiable的全局范围
+
+        //或者load
+        //$posts->load('user');
+
         return view('/admin/post/index', compact('posts'));
     }
 
